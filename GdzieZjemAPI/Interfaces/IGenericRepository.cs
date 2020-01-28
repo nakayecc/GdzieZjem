7499fece8a51e54cde388e77176e2a9dde1df176
Michal Nabielec <michal.nabi@gmail.com>
2020-01-28 14:08:49 +0100

    Added generic repository

---
+++ GdzieZjemAPI/Interfaces/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace GdzieZjemAPI.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        List<T> GetAll();
+        T GetById(object id);
+        void Insert(T obj);
+        void Delete(object id);
+        void Update(T obj);
+        void Save();
+    }
+}
---
+++ GdzieZjemAPI/Services/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GdzieZjemAPI.Interfaces;
+using GdzieZjemAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GdzieZjemAPI.Services
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private ApiContext _context;
+        private DbSet<T> table;
+
+        public GenericRepository(ApiContext context)
+        {
+            _context = context;
+            table = _context.Set<T>();
+        }
+
+        public List<T> GetAll()
+        {
+            return table.ToList();
+        }
+
+        public T GetById(object id)
+        {
+            return table.Find(id);
+        }
+
+        public void Insert(T obj)
+        {
+            table.Add(obj);
+        }
+
+        public void Delete(object id)
+        {
+            T exists = table.Find(id);
+            table.Remove(exists);
+        }
+
+        public void Update(T obj)
+        {
+            table.Attach(obj);
+            _context.Entry(obj).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
