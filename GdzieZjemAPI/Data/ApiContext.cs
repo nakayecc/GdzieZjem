568d1e5df90c66e649a058b55bce6e73e4f0157e
Michal <michal.nabie@gmail.com>
2020-01-30 21:36:33 +0100

    Fix OnModelCreating

--- GdzieZjemAPI/Data/ApiContext.cs
+++ GdzieZjemAPI/Data/ApiContext.cs
@@ -16,28 +16,9 @@ namespace GdzieZjemAPI.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<CityRestaurant>().HasKey(cr => new {cr.CityId, cr.RestaurantId});
-            
-            modelBuilder.Entity<CityRestaurant>()
-                .HasOne<City>(cr => cr.City)
-                .WithMany(c => c.CityRestaurants)
-                .HasForeignKey(cr => cr.CityId);
-
-            modelBuilder.Entity<CityRestaurant>()
-                .HasOne<Restaurant>(cr => cr.Restaurant)
-                .WithMany(r => r.CityRestaurants)
-                .HasForeignKey(cr => cr.CityId);
 
             modelBuilder.Entity<DishRestaurant>().HasKey(dr => new {dr.DishId, dr.RestaurantId});
-
-            modelBuilder.Entity<DishRestaurant>()
-                .HasOne<Dish>(dr => dr.Dish)
-                .WithMany(d => d.DishRestaurants)
-                .HasForeignKey(dr => dr.DishId);
-
-            modelBuilder.Entity<DishRestaurant>()
-                .HasOne<Restaurant>(dr => dr.Restaurant)
-                .WithMany(r => r.DishRestaurants)
-                .HasForeignKey(dr => dr.RestaurantId);
+            
         }
     }
 }
