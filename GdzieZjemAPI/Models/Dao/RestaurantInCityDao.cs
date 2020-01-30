7e6df70496c7b1eb013bb269e88dccbcccef8132
Michal <michal.nabie@gmail.com>
2020-01-30 22:02:28 +0100

    Added RestaurantCityDao

---
+++ GdzieZjemAPI/Models/Dao/RestaurantInCityDao.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace GdzieZjemAPI.Models.Dao
+{
+    public class RestaurantInCityDao
+    {
+        public string City { get; set; }
+        public List<SelectRestaurantDao> Restaurant { get; set; }
+        public RestaurantInCityDao()
+        {
+            Restaurant = new List<SelectRestaurantDao>();
+        }
+    }
+}
