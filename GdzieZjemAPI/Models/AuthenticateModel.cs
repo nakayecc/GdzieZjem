28c70639318a3a3c8779499486b9ad385e8a88d4
Michal <michal.nabie@gmail.com>
2020-02-11 00:54:37 +0100

    Added Auth model

---
+++ GdzieZjemAPI/Models/AuthenticateModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GdzieZjemAPI.Models
+{
+    public class AuthenticateModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
