d3a303bd7dd0de9cb11730b5939b5add0775624f
Michal <michal.nabie@gmail.com>
2020-02-11 00:55:22 +0100

    Added Token controller

---
+++ GdzieZjemAPI/Controllers/TokenController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using GdzieZjemAPI.Interfaces;
+using GdzieZjemAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace GdzieZjemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : Controller
+    {
+        private readonly ITokenService _tokenService;
+
+        public TokenController(ITokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        // Post
+        [HttpPost("auth")]
+        public IActionResult Authenticate([FromBody]AuthenticateModel model)
+        {
+            var user = _tokenService.AddTokenToUser(model.Username, model.Password);
+            if(user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+            return Ok(user);
+        }
+        
+        
+    
+    }
+}
