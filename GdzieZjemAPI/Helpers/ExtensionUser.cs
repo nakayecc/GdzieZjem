04551cfda603c2b1087d83dfb268c941497d9e83
Michal <michal.nabie@gmail.com>
2020-02-11 00:55:54 +0100

    Added Extension

---
+++ GdzieZjemAPI/Helpers/ExtensionUser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GdzieZjemAPI.Models;
+
+namespace GdzieZjemAPI.Helpers
+{
+    public static class ExtensionUser
+    {
+        public static IEnumerable<User> WithoutPasswords(this IEnumerable<User> users)
+        {
+            return users.Select(user => user.WithoutPassword());
+        }
+
+        private static User WithoutPassword(this User user)
+        {
+            user.Password = null;
+            return user;
+        }
+    }
+}
