c4b6771b55323b86b6565f2a3fef3094cf2ad259
Michal Nabielec <michal.nabi@gmail.com>
2020-02-24 10:22:19 +0100

    Added RestaurantFullLocalization

---
+++ GdzieZjemAPI/Models/Dao/RestaurantFullLocalization.cs
@@ -0,0 +1,9 @@
+﻿namespace GdzieZjemAPI.Models.Dao
+{
+    public class RestaurantFullLocalization
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+    }
+}
