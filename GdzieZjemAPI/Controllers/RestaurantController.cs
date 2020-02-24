68f208a96088f927463db9eee1e970e1671496c2
Michal Nabielec <michal.nabi@gmail.com>
2020-02-24 10:21:54 +0100

    Added Restaurant controller

---
+++ GdzieZjemAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using GdzieZjemAPI.Interfaces;
+using GdzieZjemAPI.Models;
+using GdzieZjemAPI.Models.Dao;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GdzieZjemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantController : Controller
+    {
+        private readonly IRestaurantRepository _repository;
+
+        public RestaurantController(IRestaurantRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET
+        [HttpPost("Get")]
+        public List<RestaurantFullLocalization> RestaurantGetBy( string restaurantName, string restaurantAddress)
+        {
+            return _repository.GetRestaurantFullLocalizationBy(restaurantName, restaurantAddress);
+        }
+    }
+}
